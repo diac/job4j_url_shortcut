dd37881288c1297a332df60e9877db133bf7f863
diac <nikolai.gladkikh@gmail.com>
2023-01-13 12:32:48 +0300

    Конфигурация Spring Liquibase

--- src/main/java/ru/job4j/urlshortcut/UrlShortcutApplication.java
+++ src/main/java/ru/job4j/urlshortcut/UrlShortcutApplication.java
@@ -1,9 +1,13 @@
 package ru.job4j.urlshortcut;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class UrlShortcutApplication extends SpringBootServletInitializer {
@@ -13,8 +17,15 @@ public class UrlShortcutApplication extends SpringBootServletInitializer {
 		return builder.sources(UrlShortcutApplication.class);
 	}
 
+	@Bean
+	public SpringLiquibase liquibase(DataSource ds) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:db/liquibase-changeLog.xml");
+		liquibase.setDataSource(ds);
+		return liquibase;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortcutApplication.class, args);
 	}
-
 }
