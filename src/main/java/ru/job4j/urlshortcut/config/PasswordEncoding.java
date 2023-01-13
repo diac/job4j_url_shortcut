e3a3132e985ae30a4080798e4ea5250a67359ad2
diac <nikolai.gladkikh@gmail.com>
2023-01-13 15:19:35 +0300

    Добавить шифровальщик паролей

---
+++ src/main/java/ru/job4j/urlshortcut/config/PasswordEncoding.java
@@ -0,0 +1,17 @@
+package ru.job4j.urlshortcut.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class PasswordEncoding {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
