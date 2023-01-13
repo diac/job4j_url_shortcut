4c9382c3f607c9802e875fa9089938dc0d5b32b9
diac <nikolai.gladkikh@gmail.com>
2023-01-13 12:18:33 +0300

    Init commit

---
+++ src/main/java/ru/job4j/urlshortcut/UrlShortcutApplication.java
@@ -0,0 +1,20 @@
+package ru.job4j.urlshortcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class UrlShortcutApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(UrlShortcutApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortcutApplication.class, args);
+	}
+
+}
