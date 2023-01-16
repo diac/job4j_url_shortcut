f17b676ad4bd8d2d8c963671a1db960affdc4756
diac <nikolai.gladkikh@gmail.com>
2023-01-16 12:21:03 +0300

    Добавить JavaDoc к сервисам

--- src/main/java/ru/job4j/urlshortcut/service/SimpleSiteService.java
+++ src/main/java/ru/job4j/urlshortcut/service/SimpleSiteService.java
@@ -10,14 +10,29 @@ import ru.job4j.urlshortcut.util.Passwords;
 
 import java.nio.CharBuffer;
 
+/**
+ * Сервис, реализующий логику, связанную с объектами модели Site
+ */
 @Service
 @AllArgsConstructor
 public class SimpleSiteService implements SiteService {
 
+    /**
+     * Репозиторий, через который осуществляется доступ к данным модели Site в БД
+     */
     private final SiteRepository siteRepository;
 
+    /**
+     * Класс-шифровальщик паролей
+     */
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * Зарегистрировать сайт в системе по доменному имени
+     *
+     * @param domainName Доменное имя регистрируемого сайта
+     * @return Объект SiteDto, содержащий данные нового объекта Site
+     */
     @Override
     public SiteDto register(String domainName) {
         Site site = new Site();
--- src/main/java/ru/job4j/urlshortcut/service/SiteService.java
+++ src/main/java/ru/job4j/urlshortcut/service/SiteService.java
@@ -2,7 +2,16 @@ package ru.job4j.urlshortcut.service;
 
 import ru.job4j.urlshortcut.data.dto.SiteDto;
 
+/**
+ * Сервис, реализующий логику, связанную с объектами модели Site
+ */
 public interface SiteService {
 
+    /**
+     * Зарегистрировать сайт в системе по доменному имени
+     *
+     * @param domainName Доменное имя регистрируемого сайта
+     * @return Объект SiteDto, содержащий данные нового объекта Site
+     */
     SiteDto register(String domainName);
 }
