55e8e684db5520f8214de2d51feaaa5ec59d287e
diac <nikolai.gladkikh@gmail.com>
2023-01-16 11:01:54 +0300

    Добавить комментарии к таблицам БД и их столбцам

--- src/main/resources/db/scripts/001_ddl_create_table_site.sql
+++ src/main/resources/db/scripts/001_ddl_create_table_site.sql
@@ -4,3 +4,9 @@ CREATE TABLE site (
     login VARCHAR UNIQUE NOT NULL,
     password VARCHAR UNIQUE NOT NULL
 );
+
+COMMENT ON TABLE site IS 'Сайты, зарегистрированные в системе';
+COMMENT ON COLUMN site.id IS 'Идентификатор сайта';
+COMMENT ON COLUMN site.domain_name IS 'Доменное имя сайта';
+COMMENT ON COLUMN site.login IS 'Логин сайта в системе';
+COMMENT ON COLUMN site.password IS 'Пароль сайта в системе';
--- src/main/resources/db/scripts/002_ddl_crate_table_url.sql
+++ src/main/resources/db/scripts/002_ddl_crate_table_url.sql
@@ -4,3 +4,9 @@ CREATE TABLE url (
     short_url VARCHAR UNIQUE NOT NULL,
     site_id INTEGER NOT NULL REFERENCES site(id)
 );
+
+COMMENT ON TABLE url IS 'Перечень URL, хранимых в системе';
+COMMENT ON COLUMN url.id IS 'Идентификатор URL';
+COMMENT ON COLUMN url.full_url IS 'Полный (исходный) URL';
+COMMENT ON COLUMN url.short_url IS 'Сокращенный URL';
+COMMENT ON COLUMN url.site_id IS 'Иддентификатор сайта, которому принадлежит URL (FK)';
