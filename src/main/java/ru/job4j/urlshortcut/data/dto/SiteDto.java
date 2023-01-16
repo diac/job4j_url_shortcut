9759dbdfe9b6410828b9629052d21c3066e343f8
diac <nikolai.gladkikh@gmail.com>
2023-01-16 12:14:40 +0300

    Добавить JavaDoc к SiteDto

--- src/main/java/ru/job4j/urlshortcut/data/dto/SiteDto.java
+++ src/main/java/ru/job4j/urlshortcut/data/dto/SiteDto.java
@@ -4,12 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * DTO для модели данных Site
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SiteDto {
 
+    /**
+     * Доменное имя сайта
+     */
     private String domainName;
+
+    /**
+     * Логин сайта в системе
+     */
     private String login;
+
+    /**
+     * Пароль сайта в системе
+     */
     private char[] password;
 }
