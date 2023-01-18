5d05e8542e0fa025e4565da3eefdef4829c12f93
diac <nikolai.gladkikh@gmail.com>
2023-01-18 10:52:31 +0300

    Добавить валидацию SiteRegistrationRequestDto

--- src/main/java/ru/job4j/urlshortcut/data/dto/SiteRegistrationRequestDto.java
+++ src/main/java/ru/job4j/urlshortcut/data/dto/SiteRegistrationRequestDto.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * DTO для обработки входящих запросов на регистрацию сайта
  */
@@ -15,5 +18,7 @@ public class SiteRegistrationRequestDto {
     /**
      * Доменное имя сайта
      */
+    @NotNull(message = "Site cannot be null")
+    @NotBlank(message = "Site cannot be blank")
     private String site;
 }
